133e4f92983a71d3160f5258050603f7e064451a
quinn-anaiah1 <aequinn@miners.utep.edu>
2024-07-15 11:19:08 -0600

    some changes

--- Backend/MyFunkoApi/obj/Debug/net8.0/MyFunkoApi.AssemblyInfo.cs
+++ Backend/MyFunkoApi/obj/Debug/net8.0/MyFunkoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFunkoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cbbc51ba89313da5e5d4ee245fab1ee7042b4ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFunkoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFunkoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
